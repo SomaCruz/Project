3e4c3a86cf9ca4c955fddc7ba2e4bcba74f35e32
Patrick Breukelman <p.breukelman@xs4all.nl>
2014-12-01 15:45:21 +0100

    no message

--- KantineSimulatie.java
+++ KantineSimulatie.java
@@ -15,15 +15,18 @@ public class KantineSimulatie {
     public void simuleer(int dagen) 
     {
         // for lus voor dagen
-        for(...) {
+        for(int i = 0; i < dagen; i++) {
             // per dag nu even vast 10+i personen naar binnen
             // laten gaan, wordt volgende week veranderd...
             // for lus voor personen
             for(int j=0;j<10+i;j++){
-                // kantine.(…);
+                kantine.loopPakSluitAan();
             }
             // verwerk rij voor de kassa
+            kantine.verwerkRijVoorKassa();
             // toon dagtotalen (artikelen en geld in kassa)
+            System.out.println("Totaal aantal vekochte artikelen: "+kantine.aantalArtikelen());
+            System.out.println("Totaal aantal geld in kassa: "+kantine.hoeveelheidGeldInKassa());
             // reset de kassa voor de volgende dag
             kantine.resetKassa();
         }
