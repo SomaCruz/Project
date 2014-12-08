d207c8046483bf29cf55978958f95409cbc8b888
Patrick Breukelman <p.breukelman@xs4all.nl>
2014-12-08 13:25:50 +0100

    no message

--- Kantine.java
+++ Kantine.java
@@ -50,32 +50,11 @@ public class Kantine
             kassa.rekenAf(persoon);
         }
     }
-    
-    /**
-     * Deze methode telt het geld uit de kassa
-     * @return hoeveelheid geld in kassa
-     */
-    public double hoeveelheidGeldInKassa() 
-    {
-        return kassa.hoeveelheidGeldInKassa();
-    }
-    
-    /**
-     * Deze methode geeft het aantal gepasseerde artikelen.
-     * @return het aantal gepasseerde artikelen
-     */
-    public int aantalArtikelen()
-    {
-        return kassa.aantalArtikelen();
-    }
-    
-    /**
-     * Deze methode reset de bijgehouden telling van
-     * het aantal artikelen
-     * en "leegt" de inhoud van de kassa.
-     */
-    public void resetKassa() 
+     
+    private void getKassa()
     {
         kassa.resetKassa();
+        kassa.aantalArtikelen();
+        kassa.hoeveelheidGeldInKassa();
     }
 }
